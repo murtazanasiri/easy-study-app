e2688f21e785791c016962f6927854b663b983ee
murtazanasiri <mortazanasiri1991@gmail.com>
2023-10-23 13:15:06 +0300

    Added result page with static data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import SearchBar from "./components/SearchBar";
+import Result from "./components/Result";
 
 function App() {
   return (
     <div className="App">
       <h1>📖 Easy Book Study 📚</h1>
+      {/* Render the SearchBar component */}
       <SearchBar></SearchBar>
+      {/* Render the Result component */}
+      <Result></Result>
     </div>
   );
 }
