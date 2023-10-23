02f0f6d6b3d64d998ba93fc89272909752cf6c91
murtazanasiri <mortazanasiri1991@gmail.com>
2023-10-24 02:35:17 +0300

    Implement Google Books API Integration: Fetch data from the Google Books API.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import "./App.css";
 
 import SearchBar from "./components/SearchBar";
 import Result from "./components/Result";
+import React, { useState } from "react";
 
 function App() {
+  const [searchData, setSearchData] = useState([]);
+
+  // Function to fetch data from the Google Books API
+  const fetchDataFromAPI = (searchQuery) => {
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const books = data.items.map((item) => {
+          const volumeInfo = item.volumeInfo;
+          return {
+            title: volumeInfo.title,
+            authors: volumeInfo.authors,
+            publishedDate: volumeInfo.publishedDate,
+            language: volumeInfo.language,
+            categories: volumeInfo.categories,
+            smallThumbnail: volumeInfo.imageLinks?.smallThumbnail,
+            description: volumeInfo.description,
+          };
+        });
+        setSearchData(books);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
   return (
     <div className="App">
       <h1>📖 Easy Book Study 📚</h1>
       {/* Render the SearchBar component */}
-      <SearchBar></SearchBar>
+      <SearchBar onSearch={fetchDataFromAPI}></SearchBar>
       {/* Render the Result component */}
-      <Result></Result>
+      <Result searchData={searchData}></Result>
     </div>
   );
 }
